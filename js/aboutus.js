d8341768a1771eda4d78d1c7a2ad2e757b2315f9
Acube Innovation <pramodgmenon@acube.co>
2014-02-05 12:42:41 +0530

    about us not finished

---
+++ js/aboutus.js
@@ -0,0 +1,35 @@
+$(function(){
+
+	var horizontal_slide = new Swiper('.swiper-container-horizontal',{
+        calculateHeight: false,
+        cssWidthAndHeight: true,
+		pagination:'.pagination',
+		centeredSlides: true,
+		slidesPerView: 2,
+		watchActiveIndex: true,
+		keyboardControl: true,
+		autoResize : false,	
+		speed : 600,	
+		paginationClickable: true
+		
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
