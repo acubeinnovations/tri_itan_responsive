c02945a7b7a57d9c4535236c818a6794d3bc649a
rajeshtr <rajeshtr@acube.co>
2014-02-20 16:00:22 +0530

    contactus page completed

--- js/map.responsive.js
+++ js/map.responsive.js
@@ -5,27 +5,30 @@ $(function(){
 	map_width=window_width/2;
 	
 	if(window_height==692){
-		map_height=window_height-170;
+		map_height=window_height-171;
 		$('#address-container').css('top',map_height);
+		$('#address-container').css('width',window_width-150);
 		$('#map').css('width',map_width);
 		$('#map1').css('width',map_width);
-		$('#map').css('height',map_height);
-		$('#map1').css('height',map_height);
+		$('#map').css('height',map_height-2);
+		$('#map1').css('height',map_height-2);
 	}else{
-		map_height=window_height-150;
+		map_height=window_height-152;
 		$('#map').css('width',map_width);
 		$('#map1').css('width',map_width);
 		$('#map').css('height',map_height);
 		$('#map1').css('height',map_height);
 		$('#address-container').css('top',map_height);
-	}
+	}//alert(window_width);
 	if(window_width>=768){
+	$('#address-container').css('width',window_width-150);
 	$('#address-container').css('padding-left',150);
-	$('#address-table').css('width',window_width-160);
-	$('#address-dubai').css('width',map_width-150);
-	$('#address-mumbai').css('width',map_width-10);
+	$('#address-table').css('width',window_width-152);
+	$('#address-dubai').css('width',map_width-152);
+	$('#address-mumbai').css('width',map_width);
 	}else{
 	$('#address-container').css('padding-left',0);
+	$('#address-container').css('width',window_width-150);
 	$('#address-table').css('width',window_width);
 	$('#address-dubai').css('width',map_width);
 	$('#address-mumbai').css('width',map_width);
@@ -33,7 +36,8 @@ $(function(){
 	
 	}
 	change_map_pos();
-
+	var border_color=$('body').css("background-color");
+	$('.td-class').css('border-left','1px solid '+border_color);
 	var zoom = $( window ).width();
 	$(window).resize(function() {
 		var zoomNew = $( window ).width();
